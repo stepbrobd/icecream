0f1e041909a2bd47fca3f7906fb6a54efbbd5085
Yifei Sun <ysun@hey.com>
2023-05-08 16:09:31 -0600

    change config

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ terraform {
   required_providers {
     fly = {
       source  = "fly-apps/fly"
-      version = "0.0.20"
+      version = "0.0.21"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "3.27.0"
+      version = "4.5.0"
     }
   }
 }
@@ -28,13 +28,15 @@ variable "config" {
     "public_domain_zone_id" = "b344936e258a6bd57c5e00af70fe0326"
     "public_port"           = "22"
 
-    "admin_ssh_key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw"
-
-    "region"    = "sjc"    # https://fly.io/docs/reference/regions
     "cpu_count" = "1"      # at least 1
     "cpu_type"  = "shared" # shared, standard, performance
     "ram_size"  = "512"    # MB
-    "disk_size" = "5"      # GB
+    "disk_size" = "10"     # GB
+    "region"    = "bos"    # https://fly.io/docs/reference/regions
+
+    "name"          = "StepBroBD"
+    "image"         = "charmcli/soft-serve:v0.5.2"
+    "admin_ssh_key" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ47Qtg6qSenUh6Whg3ZIpIhdZZdqdG+L1z2f9UnB+Mw"
   }
 }
 
@@ -70,17 +72,22 @@ resource "fly_machine" "machine" {
   app    = var.config["proejct_name"]
   name   = replace(format("%s-machine", var.config["proejct_name"]), "-", "_")
   region = var.config["region"]
-  image  = "charmcli/soft-serve:v0.4.5"
+  image  = var.config["image"]
 
   cpus     = tonumber(var.config["cpu_count"])
   cputype  = var.config["cpu_type"]
   memorymb = tonumber(var.config["ram_size"])
 
   env = {
-    "SOFT_SERVE_BIND_ADDRESS"      = "0.0.0.0"
-    "SOFT_SERVE_HOST"              = format("%s.%s", var.config["public_subdomain"], var.config["public_naked_domain"])
-    "SOFT_SERVE_PORT"              = "23231"
-    "SOFT_SERVE_INITIAL_ADMIN_KEY" = var.config["admin_ssh_key"]
+    "SOFT_SERVE_NAME"               = var.config["name"]
+    "SOFT_SERVE_INITIAL_ADMIN_KEYS" = var.config["admin_ssh_key"]
+    "SOFT_SERVE_DATA_PATH"          = "/data"
+
+    "SOFT_SERVE_SSH_LISTEN_ADDR" = ":22"
+    "SOFT_SERVE_SSH_PUBLIC_URL"  = format("ssh://%s.%s", var.config["public_subdomain"], var.config["public_naked_domain"])
+
+    "SOFT_SERVE_HTTP_LISTEN_ADDR" = ":443"
+    "SOFT_SERVE_HTTP_PUBLIC_URL"  = format("https://%s.%s", var.config["public_subdomain"], var.config["public_naked_domain"])
   }
 
   mounts = [{
